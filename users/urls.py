b6423f21317c071c5c174f88cc8612295aa1d494
QuvonchbekQosimov <qosimovquvonc@gmail.com>
2026-04-15 10:23:17 +0500

    04-15

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,7 @@ from django.core.checks import register
 from django.urls import path
 
 from users.views import RegisterView, LoginView
+app_name = 'users'
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
--- users/views.py
+++ users/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+
 
 
 class RegisterView(View):
     def get(self, request):
         return render(request, 'users/register.html')
+    def post(self, request):
+        user = User.objects.create_user(
+            username=request.POST['username'],
+            first_name=request.POST['first_name'],
+            last_name=request.POST['last_name'],
+            email=request.POST['email']
+        )
+        user.set_password(request.POST['password'])
+        user.save()
+        return redirect('users:login')
 
 class LoginView(View):
     def get(self, request):
