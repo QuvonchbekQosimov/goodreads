036386122f3dc29c425ff34777ad096dbcd45674
QuvonchbekQosimov <qosimovquvonc@gmail.com>
2026-05-01 10:49:30 +0500

    05-00

--- books/views.py
+++ books/views.py
@@ -1,17 +1,29 @@
-from django.shortcuts import render
-from django.views import View
+from django.views.generic import DetailView, ListView
 
 from books.models import Book
 
 
-class BooksView(View):
-    def get(self, request):
-        books = Book.objects.all()
+class BooksView(ListView):
+    model = Book
+    template_name = 'books/list.html'
+    context_object_name = 'books'
+    queryset = Book.objects.all()
 
-        return render(request, 'books/list.html', {'books': books})
+
+class BooksDetailsView(DetailView):
+    model = Book
+    template_name = 'books/details.html'
+    context_object_name = 'book'
+    pk_url_kwarg = "id"
+
+# class BooksView(View):
+#     def get(self, request):
+#         books = Book.objects.all()
+#
+#         return render(request, 'books/list.html', {'books': books})
 
 
-class BooksDetailsView(View):
-    def get(self, request, id):
-        book = Book.objects.get(id=id)
-        return render(request, 'books/details.html', {'book': book})
+# class BooksDetailsView(View):
+#     def get(self, request, id):
+#         book = Book.objects.get(id=id)
+#         return render(request, 'books/details.html', {'book': book})
